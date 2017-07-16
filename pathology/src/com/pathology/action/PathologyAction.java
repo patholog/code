7b4c7f5f4eb7306df75005e43234b579cfdead03
LiChunxue <18600168159@163.com>
2017-07-16 09:04:45 +0800

    解决待诊断列表打不开的问题

--- pathology/src/com/pathology/action/PathologyAction.java
+++ pathology/src/com/pathology/action/PathologyAction.java
@@ -11,95 +11,99 @@ import java.util.List;
 
 public class PathologyAction extends BaseAction {
 
-  private IPathologyService pathologyService;
-  private List<PathologyDTO> pathologys;
-  private PathologyDTO pathology;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private IPathologyService pathologyService;
+	private List<PathologyDTO> pathologys;
+	private PathologyDTO pathology;
 
-  /**
-   * 保存病理
-   *
-   * @param pathologgyjson
-   * @return
-   */
-  public Pathology savePathology(String pathologgyjson) {
-    JSONObject obj = new JSONObject().fromObject(pathologgyjson);//将json字符串转换为json对象
-    // 将json对象转换为java对象
-    Pathology jb = (Pathology) JSONObject.toBean(obj, Pathology.class);//将建json对象转换为Person对象
-    pathologyService.addPathology(jb);
-    return jb;
-  }
+	/**
+	 * 保存病理
+	 * 
+	 * @param pathologgyjson
+	 * @return
+	 */
+	public Pathology savePathology(String pathologgyjson) {
+		JSONObject obj = JSONObject.fromObject(pathologgyjson);// 将json字符串转换为json对象
+		// 将json对象转换为java对象
+		Pathology jb = (Pathology) JSONObject.toBean(obj, Pathology.class);// 将建json对象转换为Person对象
+		pathologyService.addPathology(jb);
+		return jb;
+	}
 
+	/**
+	 * 待诊断
+	 * 
+	 * @return
+	 */
+	public String getPathologyListToNeed() {
+		try {
+			HttpServletRequest request = ServletActionContext.getRequest();
+			pathologys = pathologyService.getListPathologyToNeed(request, "");
+			return "pathologysneed";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "pathologysneed";
+	}
 
-  /**
-   * 待诊断
-   *
-   * @return
-   */
-  public String getPathologyListToNeed() {
-    try {
-      HttpServletRequest request = ServletActionContext.getRequest();
-      pathologys = pathologyService.getListPathologyToNeed(request, "");
-      return "pathologysneed";
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return "pathologysneed";
-  }
+	/**
+	 * 查看待诊断
+	 * 
+	 * @return
+	 */
+	public String daizhenduan() {
+		HttpServletRequest request = ServletActionContext.getRequest();
+		pathology = pathologyService.getPathologyToNeed(request,
+				request.getParameter("id"));
+		return "daizhenduan";
+	}
 
-  /**
-   * 查看待诊断
-   *
-   * @return
-   */
-  public String daizhenduan() {
-    HttpServletRequest request = ServletActionContext.getRequest();
-    pathology = pathologyService.getPathologyToNeed(request, request.getParameter("id"));
-    return "daizhenduan";
-  }
+	public String getPathologyListToHas() {
+		try {
+			HttpServletRequest request = ServletActionContext.getRequest();
+			pathologys = pathologyService.getListPathologyToHas(request, "");
+			return "pathologyshas";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "pathologyshas";
+	}
 
-  public String getPathologyListToHas() {
-    try {
-      HttpServletRequest request = ServletActionContext.getRequest();
-      pathologys = pathologyService.getListPathologyToHas(request, "");
-      return "pathologyshas";
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return "pathologyshas";
-  }
+	public String getPathologyListToBack() {
+		try {
+			HttpServletRequest request = ServletActionContext.getRequest();
+			pathologys = pathologyService.getListPathologyToBack(request, "");
+			return "pathologysback";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "pathologysback";
+	}
 
-  public String getPathologyListToBack() {
-    try {
-      HttpServletRequest request = ServletActionContext.getRequest();
-      pathologys = pathologyService.getListPathologyToBack(request, "");
-      return "pathologysback";
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return "pathologysback";
-  }
+	public IPathologyService getPathologyService() {
+		return pathologyService;
+	}
 
-  public IPathologyService getPathologyService() {
-    return pathologyService;
-  }
+	public void setPathologyService(IPathologyService shareService) {
+		this.pathologyService = shareService;
+	}
 
-  public void setPathologyService(IPathologyService shareService) {
-    this.pathologyService = shareService;
-  }
+	public List<PathologyDTO> getPathologys() {
+		return pathologys;
+	}
 
-  public List<PathologyDTO> getPathologys() {
-    return pathologys;
-  }
+	public void setPathologys(List<PathologyDTO> pathologys) {
+		this.pathologys = pathologys;
+	}
 
-  public void setPathologys(List<PathologyDTO> pathologys) {
-    this.pathologys = pathologys;
-  }
+	public PathologyDTO getPathology() {
+		return pathology;
+	}
 
-  public PathologyDTO getPathology() {
-    return pathology;
-  }
-
-  public void setPathology(PathologyDTO pathology) {
-    this.pathology = pathology;
-  }
+	public void setPathology(PathologyDTO pathology) {
+		this.pathology = pathology;
+	}
 }
