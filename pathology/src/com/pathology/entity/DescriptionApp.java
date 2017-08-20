3ac6dd56ade02437acedb4a804e57394df7259f1
danqiubore <yangyang8715@126.com>
2017-08-20 18:37:45 +0800

    转诊

--- pathology/src/com/pathology/entity/DescriptionApp.java
+++ pathology/src/com/pathology/entity/DescriptionApp.java
@@ -9,228 +9,235 @@ import java.util.Date;
 
 public class DescriptionApp implements java.io.Serializable {
 
-	// Fields
+  // Fields
 
-	private Integer idDescriptionApp;
-	private String caseId;
-	private String fromHospitalId;
-	private String fromDoctorId;
-	private String toHospitalId;
-	private String toDoctorId;
-	private Timestamp applyDateTime;
-	private String transferParentId;
-	private String memo;
-	private String sortNo;
-	private String spellNo;
-	private String wubiNo;
-	private Integer updCnt;
-	private Timestamp crtTime;
-	private String crtUserId;
-	private String crtDeptCd;
-	private Date lastUpdTime;
-	private String lastUpdDeptCd;
-	private String lastUpdUserId;
-	private Integer delF;
+  private Integer idDescriptionApp;
+  private String caseId;
+  private String fromHospitalId;
+  private String fromDoctorId;
+  private String toHospitalId;
+  private String toDoctorId;
+  private Timestamp applyDateTime;
+  private Integer transferParentId;
+  private String memo;
+  private String sortNo;
+  private String spellNo;
+  private String wubiNo;
+  private Integer updCnt;
+  private Timestamp crtTime;
+  private String crtUserId;
+  private String crtDeptCd;
+  private Date lastUpdTime;
+  private String lastUpdDeptCd;
+  private String lastUpdUserId;
+  private Integer delF;
 
-	// Constructors
+  // Constructors
 
-	/** default constructor */
-	public DescriptionApp() {
-	}
+  /**
+   * default constructor
+   */
+  public DescriptionApp() {
+  }
 
-	/** minimal constructor */
-	public DescriptionApp(Integer idDescriptionApp) {
-		this.idDescriptionApp = idDescriptionApp;
-	}
+  /**
+   * minimal constructor
+   */
+  public DescriptionApp(Integer idDescriptionApp) {
+    this.idDescriptionApp = idDescriptionApp;
+  }
 
-	/** full constructor */
-	public DescriptionApp(Integer idDescriptionApp, String fromHospitalId,
-			String fromDoctorId, String toHospitalId, String toDoctorId,
-			Timestamp applyDateTime, String transferParentId, String memo,
-			String sortNo, String spellNo, String wubiNo, Integer updCnt,
-			Timestamp crtTime, String crtUserId, String crtDeptCd,
-			Date lastUpdTime, String lastUpdDeptCd, String lastUpdUserId,
-			Integer delF) {
-		this.idDescriptionApp = idDescriptionApp;
-		this.fromHospitalId = fromHospitalId;
-		this.fromDoctorId = fromDoctorId;
-		this.toHospitalId = toHospitalId;
-		this.toDoctorId = toDoctorId;
-		this.applyDateTime = applyDateTime;
-		this.transferParentId = transferParentId;
-		this.memo = memo;
-		this.sortNo = sortNo;
-		this.spellNo = spellNo;
-		this.wubiNo = wubiNo;
-		this.updCnt = updCnt;
-		this.crtTime = crtTime;
-		this.crtUserId = crtUserId;
-		this.crtDeptCd = crtDeptCd;
-		this.lastUpdTime = lastUpdTime;
-		this.lastUpdDeptCd = lastUpdDeptCd;
-		this.lastUpdUserId = lastUpdUserId;
-		this.delF = delF;
-	}
+  /**
+   * full constructor
+   */
+  public DescriptionApp(Integer idDescriptionApp, String caseId, String fromHospitalId,
+                        String fromDoctorId, String toHospitalId, String toDoctorId,
+                        Timestamp applyDateTime, Integer transferParentId, String memo,
+                        String sortNo, String spellNo, String wubiNo, Integer updCnt,
+                        Timestamp crtTime, String crtUserId, String crtDeptCd,
+                        Date lastUpdTime, String lastUpdDeptCd, String lastUpdUserId,
+                        Integer delF) {
+    this.idDescriptionApp = idDescriptionApp;
+    this.caseId = caseId;
+    this.fromHospitalId = fromHospitalId;
+    this.fromDoctorId = fromDoctorId;
+    this.toHospitalId = toHospitalId;
+    this.toDoctorId = toDoctorId;
+    this.applyDateTime = applyDateTime;
+    this.transferParentId = transferParentId;
+    this.memo = memo;
+    this.sortNo = sortNo;
+    this.spellNo = spellNo;
+    this.wubiNo = wubiNo;
+    this.updCnt = updCnt;
+    this.crtTime = crtTime;
+    this.crtUserId = crtUserId;
+    this.crtDeptCd = crtDeptCd;
+    this.lastUpdTime = lastUpdTime;
+    this.lastUpdDeptCd = lastUpdDeptCd;
+    this.lastUpdUserId = lastUpdUserId;
+    this.delF = delF;
+  }
 
-	// Property accessors
+  // Property accessors
 
-	public Integer getIdDescriptionApp() {
-		return this.idDescriptionApp;
-	}
+  public Integer getIdDescriptionApp() {
+    return this.idDescriptionApp;
+  }
 
-	public void setIdDescriptionApp(Integer idDescriptionApp) {
-		this.idDescriptionApp = idDescriptionApp;
-	}
+  public void setIdDescriptionApp(Integer idDescriptionApp) {
+    this.idDescriptionApp = idDescriptionApp;
+  }
 
-	public String getFromHospitalId() {
-		return this.fromHospitalId;
-	}
+  public String getFromHospitalId() {
+    return this.fromHospitalId;
+  }
 
-	public void setFromHospitalId(String fromHospitalId) {
-		this.fromHospitalId = fromHospitalId;
-	}
+  public void setFromHospitalId(String fromHospitalId) {
+    this.fromHospitalId = fromHospitalId;
+  }
 
-	public String getFromDoctorId() {
-		return this.fromDoctorId;
-	}
+  public String getFromDoctorId() {
+    return this.fromDoctorId;
+  }
 
-	public void setFromDoctorId(String fromDoctorId) {
-		this.fromDoctorId = fromDoctorId;
-	}
+  public void setFromDoctorId(String fromDoctorId) {
+    this.fromDoctorId = fromDoctorId;
+  }
 
-	public String getToHospitalId() {
-		return this.toHospitalId;
-	}
+  public String getToHospitalId() {
+    return this.toHospitalId;
+  }
 
-	public void setToHospitalId(String toHospitalId) {
-		this.toHospitalId = toHospitalId;
-	}
+  public void setToHospitalId(String toHospitalId) {
+    this.toHospitalId = toHospitalId;
+  }
 
-	public String getToDoctorId() {
-		return this.toDoctorId;
-	}
+  public String getToDoctorId() {
+    return this.toDoctorId;
+  }
 
-	public void setToDoctorId(String toDoctorId) {
-		this.toDoctorId = toDoctorId;
-	}
+  public void setToDoctorId(String toDoctorId) {
+    this.toDoctorId = toDoctorId;
+  }
 
-	public Timestamp getApplyDateTime() {
-		return this.applyDateTime;
-	}
+  public Timestamp getApplyDateTime() {
+    return this.applyDateTime;
+  }
 
-	public void setApplyDateTime(Timestamp applyDateTime) {
-		this.applyDateTime = applyDateTime;
-	}
+  public void setApplyDateTime(Timestamp applyDateTime) {
+    this.applyDateTime = applyDateTime;
+  }
 
-	public String getTransferParentId() {
-		return this.transferParentId;
-	}
+  public Integer getTransferParentId() {
+    return this.transferParentId;
+  }
 
-	public void setTransferParentId(String transferParentId) {
-		this.transferParentId = transferParentId;
-	}
+  public void setTransferParentId(Integer transferParentId) {
+    this.transferParentId = transferParentId;
+  }
 
-	public String getMemo() {
-		return this.memo;
-	}
+  public String getMemo() {
+    return this.memo;
+  }
 
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
+  public void setMemo(String memo) {
+    this.memo = memo;
+  }
 
-	public String getSortNo() {
-		return this.sortNo;
-	}
+  public String getSortNo() {
+    return this.sortNo;
+  }
 
-	public void setSortNo(String sortNo) {
-		this.sortNo = sortNo;
-	}
+  public void setSortNo(String sortNo) {
+    this.sortNo = sortNo;
+  }
 
-	public String getSpellNo() {
-		return this.spellNo;
-	}
+  public String getSpellNo() {
+    return this.spellNo;
+  }
 
-	public void setSpellNo(String spellNo) {
-		this.spellNo = spellNo;
-	}
+  public void setSpellNo(String spellNo) {
+    this.spellNo = spellNo;
+  }
 
-	public String getWubiNo() {
-		return this.wubiNo;
-	}
+  public String getWubiNo() {
+    return this.wubiNo;
+  }
 
-	public void setWubiNo(String wubiNo) {
-		this.wubiNo = wubiNo;
-	}
+  public void setWubiNo(String wubiNo) {
+    this.wubiNo = wubiNo;
+  }
 
-	public Integer getUpdCnt() {
-		return this.updCnt;
-	}
+  public Integer getUpdCnt() {
+    return this.updCnt;
+  }
 
-	public void setUpdCnt(Integer updCnt) {
-		this.updCnt = updCnt;
-	}
+  public void setUpdCnt(Integer updCnt) {
+    this.updCnt = updCnt;
+  }
 
-	public Timestamp getCrtTime() {
-		return this.crtTime;
-	}
+  public Timestamp getCrtTime() {
+    return this.crtTime;
+  }
 
-	public void setCrtTime(Timestamp crtTime) {
-		this.crtTime = crtTime;
-	}
+  public void setCrtTime(Timestamp crtTime) {
+    this.crtTime = crtTime;
+  }
 
-	public String getCrtUserId() {
-		return this.crtUserId;
-	}
+  public String getCrtUserId() {
+    return this.crtUserId;
+  }
 
-	public void setCrtUserId(String crtUserId) {
-		this.crtUserId = crtUserId;
-	}
+  public void setCrtUserId(String crtUserId) {
+    this.crtUserId = crtUserId;
+  }
 
-	public String getCrtDeptCd() {
-		return this.crtDeptCd;
-	}
+  public String getCrtDeptCd() {
+    return this.crtDeptCd;
+  }
 
-	public void setCrtDeptCd(String crtDeptCd) {
-		this.crtDeptCd = crtDeptCd;
-	}
+  public void setCrtDeptCd(String crtDeptCd) {
+    this.crtDeptCd = crtDeptCd;
+  }
 
-	public Date getLastUpdTime() {
-		return this.lastUpdTime;
-	}
+  public Date getLastUpdTime() {
+    return this.lastUpdTime;
+  }
 
-	public void setLastUpdTime(Date lastUpdTime) {
-		this.lastUpdTime = lastUpdTime;
-	}
+  public void setLastUpdTime(Date lastUpdTime) {
+    this.lastUpdTime = lastUpdTime;
+  }
 
-	public String getLastUpdDeptCd() {
-		return this.lastUpdDeptCd;
-	}
+  public String getLastUpdDeptCd() {
+    return this.lastUpdDeptCd;
+  }
 
-	public void setLastUpdDeptCd(String lastUpdDeptCd) {
-		this.lastUpdDeptCd = lastUpdDeptCd;
-	}
+  public void setLastUpdDeptCd(String lastUpdDeptCd) {
+    this.lastUpdDeptCd = lastUpdDeptCd;
+  }
 
-	public String getLastUpdUserId() {
-		return this.lastUpdUserId;
-	}
+  public String getLastUpdUserId() {
+    return this.lastUpdUserId;
+  }
 
-	public void setLastUpdUserId(String lastUpdUserId) {
-		this.lastUpdUserId = lastUpdUserId;
-	}
+  public void setLastUpdUserId(String lastUpdUserId) {
+    this.lastUpdUserId = lastUpdUserId;
+  }
 
-	public Integer getDelF() {
-		return this.delF;
-	}
+  public Integer getDelF() {
+    return this.delF;
+  }
 
-	public void setDelF(Integer delF) {
-		this.delF = delF;
-	}
+  public void setDelF(Integer delF) {
+    this.delF = delF;
+  }
 
-	public String getCaseId() {
-		return caseId;
-	}
+  public String getCaseId() {
+    return caseId;
+  }
 
-	public void setCaseId(String caseId) {
-		this.caseId = caseId;
-	}
+  public void setCaseId(String caseId) {
+    this.caseId = caseId;
+  }
 }
--- pathology/src/com/pathology/service/impl/DescriptionAppServiceImpl.java
+++ pathology/src/com/pathology/service/impl/DescriptionAppServiceImpl.java
@@ -5,7 +5,11 @@ import com.pathology.entity.DescriptionApp;
 import com.pathology.service.DescriptionAppService;
 import com.pathology.util.SessionAgentManager;
 import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +18,7 @@ public class DescriptionAppServiceImpl implements DescriptionAppService {
   private Logger logger = Logger.getLogger(DescriptionAppServiceImpl.class);
 
   private DescriptionAppDao descriptionAppDao;
+  private JdbcTemplate jdbcTemplate;
 
   @Override
   public List<DescriptionApp> selectForListByCaseId(String caseId) {
@@ -31,9 +36,15 @@ public class DescriptionAppServiceImpl implements DescriptionAppService {
       DescriptionApp descriptionApp = new DescriptionApp();
       descriptionApp.setCaseId(paramMap.get("caseId")[0]);
       descriptionApp.setToHospitalId(paramMap.get("toHospitalId")[0]);
-      descriptionApp.setCaseId(paramMap.get("toDoctorId")[0]);
+      descriptionApp.setToDoctorId(paramMap.get("toDoctorId")[0]);
       descriptionApp.setFromDoctorId(SessionAgentManager.getSessionAgentBean().getIdUsers());
       descriptionApp.setFromHospitalId(SessionAgentManager.getSessionAgentBean().getBelonghospital());
+      descriptionApp.setApplyDateTime(Timestamp.valueOf(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())));
+      int first = jdbcTemplate.queryForInt("select min(id_description_app) from description_app"
+          + " where case_id = '" + paramMap.get("caseId")[0] + "' order by applyDateTime");
+      if (first > 0) {
+        descriptionApp.setTransferParentId(first);
+      }
       descriptionAppDao.insert(descriptionApp);
     } catch (Exception e) {
       logger.error(e.getMessage());
@@ -42,5 +53,13 @@ public class DescriptionAppServiceImpl implements DescriptionAppService {
 
   public void setDescriptionAppDao(DescriptionAppDao descriptionAppDao) {
     this.descriptionAppDao = descriptionAppDao;
+  }
+
+  public JdbcTemplate getJdbcTemplate() {
+    return jdbcTemplate;
+  }
+
+  public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
   }
 }
